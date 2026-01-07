ca16ad015a1ee068d93cdb420c8e61a69a5af578
Cameron <cameronlillis52@gmail.com>
2026-01-07 11:55:39 -0800

    Breaking down vegas images into seperate formats, for mobile and desktop.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -28,57 +28,58 @@ export default function Home() {
   return (
     <>
       <main>
-        {/*! Background graident is broken on ultrawide resolution */}
-        <section id="home" className="hero-section">
-          <header>
-            <nav>
-              <a
-                href="#home"
-                className="no-underline text-[0.95rem] text-white hover:opacity-80 text-[0.95re"
-              >
-                Home
-              </a>
-              <a
-                href="#about"
-                className="no-underline text-[0.95rem] text-white hover:opacity-80"
-              >
-                About
-              </a>
-              {/* <a
+        <header>
+          <nav>
+            <a
+              href="#home"
+              className="no-underline text-[0.95rem] text-white hover:opacity-80 text-[0.95re"
+            >
+              Home
+            </a>
+            <a
+              href="#about"
+              className="no-underline text-[0.95rem] text-white hover:opacity-80"
+            >
+              About
+            </a>
+            {/* <a
             href="#sponsor"
             className="no-underline text-[0.95rem] text-white hover:opacity-80"
           >
             Sponsor Us!
           </a> */}
 
-              <button
-                id="login"
-                className="py-2 px-6  bg-transparent text-white font-semibold cursor-pointer"
-              >
-                <Link href="#">Login</Link>
-              </button>
-              <button
-                id="register"
-                className="py-2 px-6  bg-[#FEA70A] rounded-xl text-white font-semibold cursor-pointer"
-              >
-                <Link href="#">Register</Link>
-              </button>
-            </nav>
-            <div
-              id="social"
-              className="flex gap-4 absolute right-12 top-1/2 -translate-y-1/2"
+            <button
+              id="login"
+              className="py-2 px-6  bg-transparent text-white font-semibold cursor-pointer"
             >
-              <button className="border-0 text-[1.2rem] text-white cursor-pointer bg-transparent">
-                <Image
-                  src="/images/discord-white-icon.png"
-                  alt="Discord"
-                  width={36}
-                  height={26}
-                />
-              </button>
-            </div>
-          </header>
+              <Link href="#">Login</Link>
+            </button>
+            <button
+              id="register"
+              className="py-2 px-6  bg-[#FEA70A] rounded-xl text-white font-semibold cursor-pointer"
+            >
+              <Link href="#">Register</Link>
+            </button>
+          </nav>
+          <div
+            id="social"
+            className="flex gap-4 absolute right-12 top-1/2 -translate-y-1/2"
+          >
+            <button className="border-0 text-[1.2rem] text-white cursor-pointer bg-transparent">
+              <Image
+                src="/images/discord-white-icon.png"
+                alt="Discord"
+                width={36}
+                height={26}
+              />
+            </button>
+          </div>
+        </header>
+        {/*! Background graident is broken on ultrawide resolution */}
+        <section id="home" className="hero-section">
           <div className="hero-wrapper">
+            {/* Need to refactor text to shrink with screen size */}
             <div className="hero-title">
               <h1 className="title-org">REBEL</h1>
               <h1 className="title-org">HACKS</h1>
@@ -91,10 +92,19 @@ export default function Home() {
 
               <p className="hero-date">FRI & SAT February 20–21, 2026</p>
             </div>
-
-            <div className="hero-image-wrapper">
+            <div className="vegas-sign">
               <Image
-                src="/images/vegas-sign.svg"
+                className="vegas-sign"
+                src="/images/VegasSign.png"
+                alt="Las Vegas Neon Sign"
+                width={200}
+                height={100}
+              />
+            </div>
+            
+            <div className="hero-image-wrapper-whole">
+              <Image
+                src="/images/hero-image.svg"
                 alt="Las Vegas neon sign"
                 width={580}
                 height={100}
